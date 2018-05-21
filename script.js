cb8a8a6ca12450f7fabd66e65081e1f7a29f501d
Dil Afrin <dilafrin24@gmail.com>
2018-05-21 04:32:46 +0000

    Task-2

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 /* JavaScript code here */
-
+//Task 1
 console.log("External JS")
+
+//Task 2
+
+function sumArray(arr){
+    var sum = 0;
+    arr.forEach(function(value, index){
+        sum +=value;
+    });
+    return sum ;
+}
+console.log(sumArray([1,2,3,4,5]));
