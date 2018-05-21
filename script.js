4a34b42ae640747c947b39b99e927994b1046109
Dil Afrin <dilafrin24@gmail.com>
2018-05-21 04:42:15 +0000

    Task-3

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ function sumArray(arr){
     return sum ;
 }
 console.log(sumArray([1,2,3,4,5]));
+
+//task 3
+function checkEmail(emailString){
+    var emailFormat=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var match = emailFormat.test(emailString);
+    return match;
+}
+console.log(checkEmail("john@smith.com"));
+console.log(checkEmail("johnsmith.com"));
