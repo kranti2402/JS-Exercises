fef072ad402707c1b58caa59202a8a9a0895362f
Dil Afrin <dilafrin24@gmail.com>
2018-05-21 04:53:21 +0000

    Task 4

--- script.js
+++ script.js
@@ -21,3 +21,27 @@ function checkEmail(emailString){
 }
 console.log(checkEmail("john@smith.com"));
 console.log(checkEmail("johnsmith.com"));
+
+//Task 4
+var library = [ 
+   {
+       author: 'Bill Gates',
+       title: 'The Road Ahead',
+       readingStatus: true
+   },
+   {
+       author: 'Steve Jobs',
+       title: 'Walter Isaacson',
+       readingStatus: true
+   },
+   {
+       author: 'Suzanne Collins',
+       title:  'Mockingjay: The Final Book of The Hunger Games', 
+       readingStatus: false
+   }];
+
+
+function getReadingStatus(index){
+    return library[index].readingStatus;
+}
+console.log(getReadingStatus(1));
